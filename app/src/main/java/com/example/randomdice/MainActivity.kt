ae7b719c0c2fc133b252779ec50620005219ddf6
EdsonOttoni <edson.ottoni@gmail.com>
2021-09-01 17:32:15 -0300

    Initial commit

---
+++ app/src/main/java/com/example/randomdice/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.randomdice
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * This activity allows the user to roll a dice and view the result
+ * on the screen.
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener {
+            rollDice()
+        }
+    }
+
+    /**
+     * Roll the dice and update the screen with the result.
+     */
+    private fun rollDice() {
+        // Create new Dice object with 6 sides and roll it
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+
+        // Update the screen with the dice roll
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = diceRoll.toString()
+
+        // Update the screen with the dice roll image
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        val setDiceImage = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(setDiceImage)
+    }
+
+}
+
+class Dice(val numSides: Int) {
+    fun roll(): Int {
+        return (1..numSides).random()
+    }
+}
